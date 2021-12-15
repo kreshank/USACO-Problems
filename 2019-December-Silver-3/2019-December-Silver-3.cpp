b6dce31ecab0d52bbe0e9652bde85ce81eaa4d48
hansomecrank132@gmail.com <hansomecrank132@gmail.com>
2021-12-14 22:18:44 -0500

    Dec Silver 3

--- 2019-December-Silver-3/2019-December-Silver-3.cpp
+++ 2019-December-Silver-3/2019-December-Silver-3.cpp
@@ -5,35 +5,113 @@ LANG: C++
 */
 #include <iostream>
 #include <fstream>
-#include <algorithm>
 #include <string>
-#include <cmath>
 #include <vector>
-#include <stack>
-#include <set>
+#include <cstring>
 
 using namespace std;
+
+int N, M;
+short adj[10001][10001];
+bool v[10001];
+
+void printArr() {
+    for (int i = 1; i <= M; i++) {
+        for (int j = 1; j <= M; j++) {
+            cout << adj[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+}
+
+int pin(char c) {
+    if (c == 'H') return 1;
+    return 2;
+}
+
+int query(int A, int B) {
+    if (adj[A][B] != 0) {
+        return adj[A][B];
+    }
+    if (adj[B][A] != 0) {
+        return adj[A][B] = adj[B][A];
+    }
+    v[A] = true;
+
+    for (int i = 1; i <= N; i++) {
+        if (!v[i]) { // If not visited
+            if (adj[A][i] != 0) { // I
+                adj[i][B] = query(i, B);
+                if (adj[A][i] != adj[i][B]) {
+                    adj[A][B] = 3;
+                    adj[B][A] = 3;
+                }
+                else {
+                    adj[A][B] = adj[i][B];
+                    return adj[A][B];
+                }
+            }
+        }
+    }
+    return adj[A][B];
+}
 
 int main() {
     ofstream fout("milkvisits.out");
     ifstream fin("milkvisits.in");
 
+    fin >> N >> M;
 
+    memset(adj, 0, N * N);
 
-    return 0;
+    string line;
+    fin >> line;
+
+    // Store the color of the node
+    for (int i = 1; i <= N; i++) {
+        adj[i][i] = pin(line[i - 1]);
+    }
+
+    // Process all two-way paths
+    int A, B;
+    for (int i = 1; i < N; i++) {
+        fin >> A >> B;
+        if (adj[A][A] != adj[B][B])
+            adj[A][B] = 3;
+        else {
+            adj[A][B] = adj[A][A];
+        }
+        adj[B][A] = adj[A][B];
+    }
+
+    char c;
+    for (int i = 1; i <= M; i++) {
+        memset(v, false, sizeof v);
+        fin >> A >> B >> c;
+
+        int result = query(A, B);
+
+        if (result == 3 || result == pin(c))
+            fout << 1;
+        else
+            fout << 0;
+    }
+    fout << "\n";
+    fout.close();
 }
 
 /*
-Farmer John is planning to build N (1≤N≤10^5) farms that will be connected by N−1 roads, 
-forming a tree (i.e., all farms are reachable from each-other, and there are no cycles). 
+Farmer John is planning to build N (1≤N≤10^5) farms that will be connected by N−1 roads,
+forming a tree (i.e., all farms are reachable from each-other, and there are no cycles).
 Each farm contains a cow, whose breed is either Guernsey or Holstein.
 
-Farmer John's M friends (1≤M≤105) often come to visit him. During a visit with friend i, 
-Farmer John will walk with his friend along the unique path of roads from farm Ai to farm Bi 
+Farmer John's M friends (1≤M≤10^5) often come to visit him. During a visit with friend i,
+Farmer John will walk with his friend along the unique path of roads from farm Ai to farm Bi
 (it may be the case that Ai=Bi). Additionally, they can try some milk from any cow along the
-path they walk. Since most of Farmer John's friends are also farmers, they have very strong 
-preferences regarding milk. Some of his friends will only drink Guernsey milk, while the remainder 
-will only drink Holstein milk. Any of Farmer John's friends will only be happy if they can 
+path they walk. Since most of Farmer John's friends are also farmers, they have very strong
+preferences regarding milk. Some of his friends will only drink Guernsey milk, while the remainder
+will only drink Holstein milk. Any of Farmer John's friends will only be happy if they can
 drink their preferred type of milk during their visit.
 
 Please determine whether each friend will be happy after visiting.
@@ -46,8 +124,8 @@ The second line contains a string of length N. The ith character of the string i
 
 The next N−1 lines each contain two distinct integers X and Y (1≤X,Y≤N), indicating that there is a road between farms X and Y.
 
-The next M lines contain integers Ai, Bi, and a character Ci. 
-Ai and Bi represent the endpoints of the path walked during friend i's visit, 
+The next M lines contain integers Ai, Bi, and a character Ci.
+Ai and Bi represent the endpoints of the path walked during friend i's visit,
 while Ci is either G or H if the ith friend prefers Guernsey milk or Holstein milk.
 
 OUTPUT FORMAT (file milkvisits.out):
