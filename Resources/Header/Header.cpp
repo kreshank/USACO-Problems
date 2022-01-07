1d6cf9e5a7cbe0b38fdf8589b59b5a49cfc42c7d
Ryan Wu <hansomecrank132@gmail.com>
2022-01-07 00:50:14 -0500

    setting up my header file.

--- Resources/Header/Header.cpp
+++ Resources/Header/Header.cpp
@@ -1,20 +1,29 @@
-// Header.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+ID: krxnc
+TASK: TASKNAME
+LANG: C++
+*/
 
+//#include <bits/stdc++.h>
 #include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+//Definitions
+typedef pair<int, int> pii;
+typedef long long ll;
+
+int N, K, M;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	//ifstream fin("TASKNAME.in");
+	//ofstream fout("TASKNAME.out");
+
+	cin >> N;
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
