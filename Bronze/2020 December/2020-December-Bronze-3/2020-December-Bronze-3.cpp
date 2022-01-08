5c55eea802b4224e2450bf373021bf386e6372e2
Ryan Wu <hansomecrank132@gmail.com>
2022-01-08 16:53:09 -0500

    git example

--- Bronze/2020 December/2020-December-Bronze-3/2020-December-Bronze-3.cpp
+++ Bronze/2020 December/2020-December-Bronze-3/2020-December-Bronze-3.cpp
@@ -1,20 +1,95 @@
-// 2020-December-Bronze-3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+ID: krxnc
+TASK: Stuck in a Rut
+LANG: C++
+*/
 
+//#include <bits/stdc++.h>
 #include <iostream>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+//Definitions
+typedef pair<int, int> pii;
+typedef long long ll;
+
+int num;
+
+struct interval {
+	int x, y;
+	interval() : x(0), y(0) {};
+	interval(int x, int y) : x(x), y(y) {};
+};
+
+struct vec {
+	int x, y, id;
+	bool isN;
+	vec() : x(0), y(0), id(0), isN(false) {};
+	vec(int x, int y, int id, bool isN) : x(x), y(y), id(id), isN(isN) {};
+};
+
+// For East
+int sortByY(vec A, vec B) {
+	if (A.y != B.y)
+		return A.y < B.y;
+	return A.x < B.x;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+// For North
+int sortByX(vec A, vec B) {
+	if (A.x != B.x)
+		return A.x < B.x;
+	return A.y < B.y;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+vector<vec> E, N;
+vector<vec> v;
+vector<interval> eaten;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+	cin >> num; cin.ignore();
+	v.resize(num);
+	eaten.resize(num);
+
+	string dir;
+	for (int i = 0; i < num; i++) {
+		cin >> dir;
+		v[i].id = i;
+		cin >> v[i].x >> v[i].y;
+		if (v[i].isN)
+			N.push_back(v[i]);
+		else
+			E.push_back(v[i]);
+	}
+
+	sort(N.begin(), N.end(), sortByX);
+	sort(E.begin(), E.end(), sortByY);
+
+
+
+
+	return 0;
+}
+
+/*
+Case:
+
+12
+E 3 5
+E 7 5
+E 3 9
+N 5 3
+E 4 6
+E 7 6
+E 4 10
+E 10 4
+N 11 2
+N 13 2
+N 11 6
+N 8 1
+*/
